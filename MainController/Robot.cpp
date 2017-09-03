3bbae2ebb8bf9932f02a050671578222573e5255
Liam Kinne <liamkinne@users.noreply.github.com>
2017-09-03 23:09:09 +1000

    update stuff

--- MainController/Robot.cpp
+++ MainController/Robot.cpp
@@ -1,25 +1,45 @@
 #include "Robot.h"
 
 Robot::Robot():
-	RobotBase()
+	RobotBase(),
+	ultraLeft(4, 5),
+	ultraRight(A2, A1),
+	lightLeft(A0),
+	lightRight(A1),
+	mpu(0x68),
+	omni(I2C_ADDR_OMNI)
 	{
-  
+	
 }
 
 void Robot::setup() {
+	mpu.initI2C();
+
+	pinMode(PIN_ENABLE_LED, OUTPUT);
+	pinMode(PIN_ENABLE_SWITCH, INPUT_PULLUP);
 }
 
 void Robot::loop() {
+	setState(state_t(digitalRead(3)));
+	SerialUSB.println(ultraRight.get());
 }
 
 void Robot::disabledSetup() {
+	digitalWrite(PIN_ENABLE_LED, LOW);
+
+	omni.disable();
 }
 
 void Robot::disabledLoop() {
 }
 
 void Robot::enabledSetup() {
+	digitalWrite(PIN_ENABLE_LED, HIGH);
+
+	omni.enable();
 }
 
 void Robot::enabledLoop() {
+
+	omni.set(0, 1.0);
 }
--- MainController/Robot.h
+++ MainController/Robot.h
@@ -1,17 +1,31 @@
 #ifndef ROBOT_H
 #define ROBOT_H
 
-#include <Arduino.h>
-#undef max
-#undef min
 #include <Phantom.h>
-#include <Wire.h>
+#include <Arduino.h>
 #include "RobotMap.h"
 #include <RobotBase.h>
 
-class Robot: public Phantom::RobotBase {
+#include <Wire.h>
+#include <Ultrasonic.h>
+#include <SenseLight.h>
+#include <MPU9250.h>
+#include "OmniViaI2C.h"
+
+class Robot:
+	public Phantom::RobotBase {
 public:
 	Robot();
+
+	// Sensors
+	Phantom::Ultrasonic ultraLeft;
+	Phantom::Ultrasonic ultraRight;
+	Phantom::SenseLight lightLeft;
+	Phantom::SenseLight lightRight;
+	Phantom::MPU9250 mpu;
+
+	// Motors
+  	OmniViaI2C omni;
 
 	void setup();
 	void loop();
