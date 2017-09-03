9d086d7752f158de40c55c3a5c2fb2c131b890df
Liam Kinne <liamkinne@users.noreply.github.com>
2017-09-03 15:35:16 +1000

    removed main() declaration

--- MotorController/MotorController.ino
+++ MotorController/MotorController.ino
@@ -4,34 +4,23 @@
 using namespace Phantom;
 
 Robot robot;
-
-int main(void) {
-	init();
-
-	setup();
-
-	robot.setup();
-
-	while(1) {
-		loop();
-		robot.loop();
-		robot.update();
-	}
-	return 0;
-}	
-
+	
 void setup() {
 	TCCR0B = TCCR0B & B11111000 | B00000010;
 	TCCR1B = TCCR1B & B11111000 | B00000010;
 	TCCR2B = TCCR2B & B11111000 | B00000010;
-
 	// I2C
 	Wire.begin(8);
 	Wire.onReceive(wireReceive);
+	// Serial
+	Serial.begin(57600);
+
+	robot.setup();
 }
 
 void loop() {
-
+	robot.loop();
+	robot.update();
 }
 
 byte wireBytes[8] = {};
